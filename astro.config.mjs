eeeeadd2013482c7f196affdd39552424e0baec7
tokkiperwinks <rohnmaulawin24@gmail.com>
2025-08-01 16:23:29 +0800

    ava

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tokkiperwinks.github.io/commissions/',
-  base: '/commissions/', // <-- IMPORTANT
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  base: '/commissions/',
 });
