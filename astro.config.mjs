0fc929965be24372374921b596e2e6db91c5eb58
tokkiperwinks <rohnmaulawin24@gmail.com>
2025-08-01 16:17:54 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tokkiperwinks.github.io/commissions/',
+  base: '/tokkicommissions/', // <-- IMPORTANT
   vite: {
     plugins: [tailwindcss()]
   }
